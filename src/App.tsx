01d68aa5d8300ad22875e473b8cced12a82fd926
Kingsley022 <kingsleyokereke911@gmail.com>
2025-02-13 11:00:20 +0100

    Designed the desktop side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import SideMenu from "./components/SideMenu"
+
 function App() {
 
   return (
-    <h1 className="text-2xl text-green-400">Hello World</h1>
+    <div className="grid col-span-12 bg-black h-[100vh] overflow-hidden">
+      <SideMenu/>
+
+      <main className="col-span-11"></main>
+    </div>
   )
 }
 
---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,88 @@
+import { GoHomeFill } from "react-icons/go";
+import logo from "../assets/images/white-text-logo.png"
+import { CiSearch } from "react-icons/ci";
+import { MdOutlineExplore } from "react-icons/md";
+import { BiMoviePlay } from "react-icons/bi";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { FaRegHeart, FaRegUser } from "react-icons/fa";
+import { CgAddR } from "react-icons/cg";
+import { FiMenu } from "react-icons/fi";
+
+const SideMenu = () => {
+    const menuItems = [
+        {
+            id: 1,
+            title: "Home",
+            icon: GoHomeFill,
+            pathname: "/"
+        },
+
+        {
+            id: 2,
+            title: "Search",
+            icon: CiSearch
+        },
+
+        {
+            id: 3,
+            title: "Explore",
+            icon: MdOutlineExplore
+        },
+
+        {
+            id: 4,
+            title: "Reels",
+            icon: BiMoviePlay
+        },
+
+        {
+            id: 5,
+            title: "Messages",
+            icon: IoChatbubbleEllipsesOutline
+        },
+
+        {
+            id: 6,
+            title: "Notifications",
+            icon: FaRegHeart
+        },
+
+        {
+            id: 7,
+            title: "Create",
+            icon: CgAddR
+        },
+
+        {
+            id: 8,
+            title: "Profile",
+            icon: FaRegUser
+        },
+
+        {
+            id: 9,
+            title: "More",
+            icon: FiMenu
+        },
+    ];
+
+    return (
+        <aside className="col-span-1 h-[100vh] px-6 py-12 flex flex-col gap-6 border-r-1 border-r-[#8080804b]">
+            <img src={logo} alt="instagram logo" className="w-[120px] object-contain mx-2"/>
+            <div className="flex flex-col">
+                {
+                    menuItems.map(item => (
+                        <div 
+                            key={item.id}
+                            className="flex text-white items-center px-2 py-3 gap-3 hover:bg-[#161616] transition-all rounded cursor-pointer">
+                            <item.icon className="text-2xl"/>
+                            <span style={{fontWeight: item?.pathname == "/" ? "bold" : ""}}>{item.title}</span>
+                        </div>
+                    ))
+                }
+            </div>
+        </aside>
+    )
+}
+
+export default SideMenu
