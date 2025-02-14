126ab2ffe748f1ca7cfdb51ddab93903fff46f69
Kingsley022 <kingsleyokereke911@gmail.com>
2025-02-14 09:14:17 +0100

    Created posts ui template

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
+import Posts from "./Posts"
 import Stories from "./Stories"
 
 const Main = () => {
 
     return (
-        <main className="col-span-10 flex">
+        <main className="col-span-10 flex overflow-y-scroll">
             <div className="text-white w-[70%]">
                 <Stories/>
+                <Posts/>
             </div>
 
             <div className="bg-amber-300 w-[30%]">Hello</div>
